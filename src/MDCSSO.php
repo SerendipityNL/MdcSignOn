c5a1f2b46ff4a8c70b171d5b84a59f67e35ee00c
Ramon J. A. Smit <ramon@daltcore.com>
2017-12-14 20:05:24 +0100

    Fixed bug in oauth handler

--- src/MDCSSO.php
+++ src/MDCSSO.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Closure;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
 
 class MDCSSO
@@ -19,27 +20,13 @@ class MDCSSO
      */
     public function handle($request, Closure $next)
     {
-
         if ($request->session()->has('user')) {
             Auth::login(unserialize($request->session()->get('user')));
         } else {
             return redirect()->to(config('mdc-sign-on.url').'/api/v1/oauth/auth/login/'.config('mdc-sign-on.app_token'));
         }
 
-        $client = new Client([
-            // Base URI is used with relative requests
-            'base_uri' => config('mdc-sign-on.url'),
-            // You can set any number of default request options.
-            'timeout' => 2.0,
-        ]);
-
-        $response = $client->request(
-            'GET',
-            'api/v1/oauth/auth/get/' . auth()->user()->token . '/' . config('mdc-sign-on.app_token'),
-            ['verify' => false]
-        );
-
-        $oauth = json_decode($response->getBody()->getContents());
+        $oauth = $this->handleOAuth();
 
         if (isset($oauth->code) && $oauth->code == 403) {
             return redirect()->to($oauth->login_url);
@@ -49,5 +36,34 @@ class MDCSSO
         $user->update(['token' => $oauth->uuid]);
 
         return $next($request);
+    }
+
+    /**
+     * Handle oauth call
+     * @return mixed
+     */
+    protected function handleOAuth()
+    {
+        return Cache::remember('MDC-SSO-OAUTH', 3600, function () {
+            $client = new Client([
+                // Base URI is used with relative requests
+                'base_uri' => config('mdc-sign-on.url'),
+                // You can set any number of default request options.
+                'timeout' => 2.0,
+            ]);
+
+            $response =  $client->request(
+                'GET',
+                'api/v1/oauth/auth/get/' . auth()->user()->token . '/' . config('mdc-sign-on.app_token'),
+                ['verify' => false]
+            );
+
+            try {
+                return json_decode($response->getBody()->getContents());
+            } catch (\Exception $e) {
+                Cache::forget('MDC-SSO-OAUTH');
+                $this->handleOAuth();
+            }
+        });
     }
 }
