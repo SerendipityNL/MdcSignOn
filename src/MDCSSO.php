c66397af92fcf746d688863b4f6eaa4768e49212
Ramon Smit <rsmit@daltcore.com>
2018-06-04 21:01:12 +0200

    Fixed SSO?

--- src/MDCSSO.php
+++ src/MDCSSO.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Closure;
 use GuzzleHttp\Client;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
 
 class MDCSSO
@@ -26,20 +27,9 @@ class MDCSSO
             return redirect()->to(config('mdc-sign-on.url').'/api/v1/oauth/auth/login/'.config('mdc-sign-on.app_token'));
         }
 
-        $client = new Client([
-            // Base URI is used with relative requests
-            'base_uri' => config('mdc-sign-on.url'),
-            // You can set any number of default request options.
-            'timeout' => 2.0,
-        ]);
+        $oauth = $this->handleOAuth();
 
-        $response = $client->request(
-            'GET',
-            'api/v1/oauth/auth/get/' . auth()->user()->token . '/' . config('mdc-sign-on.app_token'),
-            ['verify' => false]
-        );
 
-        $oauth = json_decode($response->getBody()->getContents());
 
         if (isset($oauth->code) && $oauth->code == 403) {
             return redirect()->to($oauth->login_url);
@@ -50,4 +40,34 @@ class MDCSSO
 
         return $next($request);
     }
+
+    /**
+     * Handle oauth call
+     * @return mixed
+     */
+    protected function handleOAuth()
+    {
+        return Cache::remember('MDC-SSO-OAUTH', 60, function () {
+            $client = new Client([
+                // Base URI is used with relative requests
+                'base_uri' => config('mdc-sign-on.url'),
+                // You can set any number of default request options.
+                'timeout' => 2.0,
+            ]);
+
+            $response =  $client->request(
+                'GET',
+                'api/v1/oauth/auth/get/' . auth()->user()->token . '/' . config('mdc-sign-on.app_token'),
+                ['verify' => false]
+            );
+
+            try {
+                return json_decode($response->getBody()->getContents());
+            } catch (\Exception $e) {
+                Cache::forget('MDC-SSO-OAUTH');
+                $this->handleOAuth();
+            }
+        });
+    }
+
 }
